2d3971b789c1c8a95b41d663f271d6572c7b8bd3
sandro-silva-de-paula <sandrosilvadepaula.usa@gamil.com>
2024-01-17 17:21:58 -0500

    acrescentado INSERT INTO

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
-
+TABLE_NAME = 'customers'
 
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
@@ -17,12 +17,23 @@ with connection:
     with connection.cursor() as cursor:
         # sql
         cursor.execute(
-            'CREATE TABLE IF NOT EXISTS customers('
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(55) NOT NULL, '
             'idade INT NOT NULL, '
             'PRIMARY KEY (id)'
             ') '
         )
+    connection.commit()
 
-# teste git
+    # Inserir dados na tgabela
+    with connection.cursor() as cursor:
+        # sql
+        sql = (f'INSERT INTO {TABLE_NAME}  (nome, idade) VALUES (%s, %s) ')
+        data = ('Alissa', 29)
+        # nao enviar valores junto com a string do sql / injection sql
+        result = cursor.execute(sql, data)
+
+        print(result)
+        print(sql, data)
+    connection.commit()
